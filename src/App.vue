0bbe76dd11d09472636ea0c10ab11d7cc0a40a94
Vinícius Cavalcanti <vhpcavalcanti@outlook.com>
2021-03-08 18:01:12 -0300

    templates navbar e formulario

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <Navbar/>
+    <v-container>
+      <v-form ref="form" v-model="valid">
+          <v-col md="4">
+            <v-text-field v-model="url" label="Url" :rules="urlRules" required></v-text-field>
+          </v-col>
+          <v-col md="4">
+            <v-text-field v-model="sheetName" label="Sheet name" :rules="sheetNameRules" required></v-text-field>
+          </v-col>
+          <v-col md="4">
+            <v-text-field v-model="range" label="Range" :rules="rangeRules" required></v-text-field>
+          </v-col>
+          <v-col md="4">
+            <v-btn 
+              :disabled="!valid"
+              color="success"
+              class="mr-4">Send
+            </v-btn>
+          </v-col>
+      </v-form>
+    </v-container>
+  </v-app>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Navbar from '@/components/Navbar'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: 'App',
+    components: {
+      Navbar
+    },
+    data: () => ({
+      valid: true,
+      url: '',
+      urlRules: [
+        v => !!v || 'Url is required'
+      ],
+      sheetName: '',
+      sheetNameRules: [
+        v => !!v || 'Sheet name is required'
+      ],
+      range: '',
+      rangeRules: [
+        v => !!v || 'Range is required'
+      ]
+    })
+  };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+    <v-card flat>
+        <v-toolbar color="purple">
+            <v-spacer />
+            <v-toolbar-title class="white--text">Sheet app</v-toolbar-title>
+            <v-spacer />
+        </v-toolbar>
+    </v-card>
+</template>
+
+
