4704ec21237d99806b254cdac3e77986e4d38364
Vinícius Cavalcanti <vhpcavalcanti@outlook.com>
2021-03-09 22:39:17 -0300

    permitindo insercao de conjunto de faixas

--- src/App.vue
+++ src/App.vue
@@ -6,22 +6,30 @@
         <v-flex xs12 md6>
           <v-card class="pa-2" flat>
             <v-form ref="form" v-model="valid">
-                <v-col md="auto">
+              <v-row>
+                <v-col cols="12" md="6">
                   <v-text-field v-model="url" label="Url" :rules="urlRules" required></v-text-field>
                 </v-col>
-                <v-col md="auto">
+                <v-col cols="12" md="6">
                   <v-text-field v-model="sheetName" label="Sheet name" :rules="sheetNameRules" required></v-text-field>
                 </v-col>
-                <v-col md="auto">
-                  <v-text-field v-model="range" label="Range" :rules="rangeRules" required></v-text-field>
+                <v-col v-for="(input, index) in ranges" :key="`rangeInput-${index}`" cols="12" md="6">
+                  <v-text-field v-model="input.range" label="Range" :rules="rangeRules" required></v-text-field>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" class="ml-2" style="cursor: pointer; white-space: nowrap;" @click="addField(input, ranges)">
+                    <path fill="none" d="M0 0h24v24H0z"/>
+                    <path fill="green" d="M11 11V7h2v4h4v2h-4v4h-2v-4H7v-2h4zm1 11C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16z"/>
+                  </svg>
+                  <svg v-show="ranges.length > 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" class="ml-2" style="cursor: pointer; white-space: nowrap;" @click="removeField(index, ranges)">
+                    <path fill="none" d="M0 0h24v24H0z"/>
+                    <path fill="#EC4899" d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm0-9.414l2.828-2.829 1.415 1.415L13.414 12l2.829 2.828-1.415 1.415L12 13.414l-2.828 2.829-1.415-1.415L10.586 12 7.757 9.172l1.415-1.415L12 10.586z"/>
+                  </svg>
                 </v-col>
-                <v-col md="auto">
-                  <v-btn 
-                    :disabled="!valid"
-                    color="success"
-                    class="mr-4">Send
-                  </v-btn>
+              </v-row>
+              <v-row>
+                <v-col md="6">
+                  <v-btn :disabled="!valid" color="success" class="mr-4">Send</v-btn>
                 </v-col>
+              </v-row>
             </v-form>
           </v-card>
         </v-flex>
@@ -68,7 +76,16 @@
       range: '',
       rangeRules: [
         v => !!v || 'Range is required'
-      ]
-    })
+      ],
+      ranges: [{range: ""}]
+    }),
+    methods: {
+      addField(value, fieldType) {
+        fieldType.push({ value: "" });
+      },
+      removeField(index, fieldType) {
+        fieldType.splice(index, 1);
+      }
+    }
   };
 </script>
